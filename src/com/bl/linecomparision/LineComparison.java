594e40f498625aea98aa238f0404b583e3b6f898
saurabh <mrsaurabhshinde5@gmail.com>
2023-02-11 14:04:52 +0530

    compare the line greater smaller or same

--- src/com/bl/linecomparision/LineComparison.java
+++ src/com/bl/linecomparision/LineComparison.java
@@ -1,6 +1,32 @@
 package com.bl.linecomparision;
 
+import java.util.Scanner;
+
 public class LineComparison
 {
+    public static void lineComparision() {
 
+        Scanner sc = new Scanner(System.in);       //create object for scanner class
+
+        System.out.println("Enter the value of x1: ");
+        int x1 = sc.nextInt();
+
+        System.out.println("Enter the value of x2: ");
+        int x2 = sc.nextInt();
+
+        System.out.println("Enter the value of y1: ");
+        int y1 = sc.nextInt();
+
+        System.out.println("Enter the value of y2: ");
+        int y2 = sc.nextInt();
+
+        double length1 = Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+        System.out.println("Length of the line is: " + length1);
+
+    }
+
+    public static void main(String[] args) {
+
+        lineComparision();         //main method
+    }
 }
